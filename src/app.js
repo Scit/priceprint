6b4812605a5db24798c5f9e7759e89275b57eac7
Andrey Terenkov <andrey.terenkov@gmail.com>
2016-11-19 17:40:51 +0300

    Add split pane

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import Frame from './components/Frame'
+import Split from 'react-split-pane'
+import '../assets/split-styles.css'
 
 export default class App extends React.Component {
   render() {
     return (
-    	<Frame>
-				<h1>Hello!!</h1>
+      <Frame>
+        <Split split="vertical" minSize={50} defaultSize={700} className="primary">
+          <div>
+            <p>left</p>
+          </div>
+          <div>
+            <p>right</p>
+          </div>
+        </Split>
       </Frame>
     )
   }
